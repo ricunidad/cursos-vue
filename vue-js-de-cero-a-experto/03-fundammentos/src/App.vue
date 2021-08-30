5fba7b02b1685fa94037aa71a7cc51bd62ce4b27
Reynaldo Rodrigo Choque Vicente <reynaldo.choque@defensoria.gob.bo>
2021-08-30 11:49:55 -0400

    Terminados Sección 4 y 5

---
+++ vue-js-de-cero-a-experto/03-fundammentos/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <Counter title="Entraron" :start="101" /> -->
+  <Indecision />
+</template>
+
+<script>
+import Counter from './components/Counter.vue'
+import Indecision from './components/Indecision.vue'
+export default {
+  name: 'App',
+  components: {
+    //Counter : Counter,
+    Indecision: Indecision,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-js-de-cero-a-experto/03-fundammentos/src/components/Counter.vue
@@ -0,0 +1,56 @@
+<template>
+  <h2>{{ customTitle }}</h2>
+  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+
+  <div>
+      <button v-on:click="increase">+1</button>
+      <button @click="decrease">-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+    // props: ['title', 'start'],
+    props: {
+        title: String,
+        start: {
+            type: Number,
+            default: 10,
+            //required: true,
+            validator(value){
+                return value > 100
+            }
+        },
+    },
+    data() {
+        return {
+            counter: this.start
+        }
+    },
+    methods: {
+        getSquareValue() {
+            console.log("getSquareValue")
+            return this.counter * this.counter
+        },
+        increase() {
+            this.counter ++;
+        },
+        decrease() {
+            this.counter --;
+        }
+    },
+    computed: {
+        squareCounter(){
+            console.log("squareCounter")
+            return this.counter * this.counter
+        },
+        customTitle(){
+            return this.title ? this.title : 'Counter'
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
